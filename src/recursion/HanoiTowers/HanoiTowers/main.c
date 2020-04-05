cc6d78cb6a800b4597bb4b35420cc64fc7f82e36
Ignacio Herrera <ignacio@amalgama.co>
2020-04-05 11:48:09 -0300

    Hanoi Towers Recursion

---
+++ src/recursion/HanoiTowers/HanoiTowers/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// A = from
+// B = intermediate
+// C = destination
+void TOH(int n, int A, int B, int C)
+{
+    if (n > 0)
+    {
+        TOH(n - 1, A, C, B);
+        printf("from %d to %d \n", A, C);
+        TOH(n - 1, B, A, C);
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    TOH(4, 1, 2, 3);
+    return 0;
+}

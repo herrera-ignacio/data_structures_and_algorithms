d8e30200be4fb1548ec32579cadd9612f8c3302c
Ignacio Herrera <ignacio@amalgama.co>
2020-04-06 21:21:59 -0300

    Array ADT in C

--- src/data-structures/array/ADT/ADT/main.c
+++ src/data-structures/array/ADT/ADT/main.c
@@ -80,21 +80,187 @@ int BinarySearch(struct Array arr, int key)
     return -1;
 }
 
+int Get(struct Array arr, int index)
+{
+    if (index >= 0 && index < arr.length)
+    {
+        return arr.A[index];
+    }
+    return -1;
+}
+
+void Set(struct Array *arr, int index, int x)
+{
+    if (index >= 0 && index < arr->length)
+    {
+        arr->A[index] = x;
+    }
+}
+
+int Max(struct Array arr)
+{
+    int max = arr.A[0];
+    int i;
+    for (i = 1; i < arr.length; i++)
+    {
+        if (arr.A[i] > max)
+            max = arr.A[i];
+    }
+    return max;
+}
+
+int Min(struct Array arr)
+{
+    int min = arr.A[0];
+    int i;
+    for (i = 1; i < arr.length; i++)
+    {
+        if (arr.A[i] < min)
+            min = arr.A[i];
+    }
+    return min;
+}
+
+int Sum(struct Array arr)
+{
+    int s = 0;
+    int i;
+    for (i = 0; i < arr.length; i++)
+        s += arr.A[i];
+    return s;
+}
+
+float Avg(struct Array arr)
+{
+    return (float)Sum(arr) / arr.length;
+}
+
+void Reverse(struct Array *arr)
+{
+    int *B;
+    int i, j;
+    
+    B = (int *)malloc(arr->length * sizeof(int));
+    
+    for (i = arr->length - 1, j = 0; i >= 0; i--, j++)
+        B[j] = arr->A[i];
+    
+    for (i = 0; i < arr->length; i++)
+        arr->A[i] = B[i];
+}
+
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void Reverse2(struct Array *arr)
+{
+    int i, j;
+    for (i = 0, j = arr->length - 1; i < j; i++, j--)
+    {
+        swap(&arr->A[i], &arr->A[j]);
+    }
+}
+
+int isSorted(struct Array arr)
+{
+    int i;
+    for (i = 0; i < arr.length - 1; i++)
+    {
+        if (arr.A[i] > arr.A[i + 1])
+            return 0;
+    }
+    return 1;
+}
+
+// preserves orden
+struct Array* Merge(struct Array *arr1, struct Array *arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+    
+    struct Array *arr3 = (struct Array *)malloc(sizeof(struct Array));
+    
+    while (i < arr1->length && j < arr2->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+            arr3->A[k++] = arr1->A[i++];
+        else
+            arr3->A[k++] = arr2->A[j++];
+    }
+    
+    for (; i < arr1->length; i++)
+        arr3->A[k++] = arr1->A[i];
+    for (; j < arr2->length; j++)
+        arr3->A[k++] = arr2->A[j];
+    arr3->length = arr1->length + arr2->length;
+    arr3->size = 10;
+    
+    return arr3;
+}
+
+// works only in sorted array
+struct Array* Intersection(struct Array *arr1, struct Array *arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+    
+    struct Array *arr3 = (struct Array *)malloc(sizeof(struct Array));
+    
+    while (i < arr1->length && j < arr->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+            i++;
+        else if (arr1->A[i] < arr2->A[j])
+            j++;
+        else if (arr1->A[i] == arr2->A[j])
+        {
+            arr3->A[k++] = arr1->A[i++];
+            j++;
+        }
+    }
+    
+    arr3->length = k;
+    arr3->size = 10;
+    
+    return arr3;
+}
+
+// works only in sorted array
+struct Array* Difference(struct Array *arr1, struct Array *arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+    
+    struct Array *arr3 = (struct Array *)malloc(sizeof(struct Array));
+    
+    while (i < arr1->length && j < arr2->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+            arr3->A[k++] = arr1->A[i++];
+        else if (arr2->A[j] < arr1->A[i])
+            j++;
+        else if (arr1->A[i] == arr2->A[j])
+        {
+            i++;
+            j++;
+        }
+    }
+    
+    for (; i < arr1->length; i++)
+        arr3->A[k++] = arr1->A[i]
+    
+    arr3->length = k;
+    arr3->size = 10;
+    
+    return arr3;
+}
+
 int main()
 {
-    struct Array arr = {{2,3,4,5,6}, 10, 5};
-    
-    Append(&arr, 7);
-    Insert(&arr, 6, 8);
-    Display(arr);
-    
-    printf("\nDeleted element [%d]: %d \n", 2, Delete(&arr, 2));
-    Display(arr);
-    
-    printf("\nLinear Search for 15: index %d\n", LinearSearch(arr, 15));
-    printf("\nLinear Search for 7: index %d\n", LinearSearch(arr, 7));
-    
-    printf("\nBinary Search for 7: index %d\n", BinarySearch(arr, 7));
-    
+    printf("Play around with any function from this Array ADT");
     return 0;
 }

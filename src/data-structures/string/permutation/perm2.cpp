708d4c793a5b33115877b6694c0ca3f941877207
Ignacio Herrera <ignacioromanherrera@gmail.com>
2020-04-25 01:31:33 -0300

    String:Permutations another solution

---
+++ src/data-structures/string/permutation/perm2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+void swapChars(std::string &s, int i, int j)
+{
+	auto temp = s[i];
+	s[i] = s[j];
+	s[j] = temp;
+}
+
+void PrintPermutations(std::string &s, int low, int high)
+{
+	if (low == high)
+	{
+		std::cout << s << std::endl;
+	}
+	else
+	{
+		int i;
+		for (i = low; i < s.size(); i++)
+		{
+			swapChars(s, i, low); // swap ith char with (low)th char
+			PrintPermutations(s, low+1, high);
+			swapChars(s, i, low); // once tree's branch is done, reverse change
+		}
+	}
+}
+
+
+int main()
+{
+	std::string test = "ABC";
+	PrintPermutations(test, 0, test.size());
+	return 0;
+}
